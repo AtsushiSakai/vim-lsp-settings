29a36a3e6c6c331383374708b67b0c5bbf19d360
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-07-16 23:17:31 +0900

    add env_path

--- installer/install-julia-language-server.sh
+++ installer/install-julia-language-server.sh
@@ -7,7 +7,8 @@ julia -e 'using Pkg; Pkg.add("LanguageServer")'
 cat <<EOF >julia-language-server
 #!/usr/bin/env bash
 
-julia -e "using LanguageServer, LanguageServer.SymbolServer; runserver()"
+julia -e "using LanguageServer, LanguageServer.SymbolServer; runserver()" $(julia -e 'print(dirname(Base.load_path_expand("@v#.#")))')
+
 EOF
 
 chmod +x julia-language-server
